4d8b02f575735c32f1bc74c9da108a8e25d876c5
godwynnn <78883893+godwynnn@users.noreply.github.com>
2025-03-23 15:27:52 +0100

    Revert "init"

--- main/paystack.py
+++ main/paystack.py
@@ -2,7 +2,7 @@ import requests
 from django.conf import settings
 
 def PaystackVerify(data,profile=None):
-    url=f"https://api.paystack.co/transaction/verify/{data['reference']}"
+    url=f'https://api.paystack.co/transaction/verify/{data['reference']}'
     headers={
         "Authorization":f"Bearer {settings.PSTACK_SECRET_KEY}"
     }
