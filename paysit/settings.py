60bbbb33107a14e04faf8f0a55cb102a72ae8968
godwynnn <miraclegodwin67@gmail.com>
2025-04-29 09:42:13 +0100

    init

--- paysit/settings.py
+++ paysit/settings.py
@@ -287,11 +287,9 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR,'static')
-# ]
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/media')
 
