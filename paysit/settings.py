87fce00a4236d4c42dd1ff86b286f2b5aaa5c6f5
Miracle Chibuzor Godwin <78883893+godwynnn@users.noreply.github.com>
2025-08-16 16:16:36 +0100

    Update settings.py

--- paysit/settings.py
+++ paysit/settings.py
@@ -291,7 +291,8 @@ DATA_DISCOUNT=0.5
 CORS_ALLOWED_ORIGINS = [
     "https://paysit-khqb.onrender.com",
     "http://localhost:3000",
-    "https://paysit-app.onrender.com"
+    "https://paysit-app.onrender.com",
+    "https://pays-it-app.vercel.app"
 ]
 
 
