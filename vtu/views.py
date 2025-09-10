15060ebddda0d9d01e28b6fdf04aa83579116785
godwynnn <miraclegodwin67@gmail.com>
2025-09-11 00:17:01 +0100

    init

--- vtu/views.py
+++ vtu/views.py
@@ -403,14 +403,16 @@ class VerifyNumberView(APIView):
                 if 'WrongBillersCode' in response['content'] and response['content']['WrongBillersCode']== True:
                     return Response({
                         'data':{},
+                        'verified':False,
                         'message':response['content']['error']
-                    }, status=status.HTTP_403_FORBIDDEN)
+                    }, status=status.HTTP_200_OK)
                     
                 elif 'error' in response['content']:
                     return Response({
                         'data':response['content'],
+                        'verified':False,
                         'message':response['content']['error']
-                    },status=status.HTTP_403_FORBIDDEN)
+                    },status=status.HTTP_200_OK)
                 
                 else:
 
@@ -426,13 +428,15 @@ class VerifyNumberView(APIView):
                     
                     return Response({
                         'data':{},
+                        'verified':False,
                         'message':f"The {service_type} Smartcard Number you entered may be invalid, Please check and only proceed if you are sure it's valid."
-                    }, status=status.HTTP_403_FORBIDDEN)
+                    }, status=status.HTTP_200_OK)
                 elif 'error' in response['content']:
                     return Response({
                         'data':response['content'],
+                        'verified':False,
                         'message':f"The {service_type} Smartcard Number you entered may be invalid, Please check and only proceed if you are sure it's valid."
-                    },status=status.HTTP_403_FORBIDDEN)
+                    },status=status.HTTP_200_OK)
                 
                 else:
 
