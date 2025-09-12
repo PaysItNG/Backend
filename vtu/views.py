bf711e7149ab15b4a843cabd64bdc6de16a58c25
godwynnn <miraclegodwin67@gmail.com>
2025-09-12 21:03:54 +0100

    init

--- vtu/views.py
+++ vtu/views.py
@@ -269,14 +269,14 @@ class VtuServicesView(APIView):
                         transaction.save()
                         data=TransactionSerializer(transaction).data
                         if response=='completed':
-                            transaction.status=response
+                            transaction.status=res_status
                             transaction.save()
                             return Response(
-                                {'data':{'status':response,"message":'ok','content':data},}, status = status.HTTP_200_OK
+                                {'data':data,"message":"Transaction successful"}, status = status.HTTP_200_OK
                             )
                         else:
                             return Response(
-                                {'data':{'status':response,"message":'','content':data}}, status = status.HTTP_400_BAD_REQUEST)
+                                {'data':data,'massage':'Transaction failed refunds will be processed shortly within 5 seconds'}, status = status.HTTP_400_BAD_REQUEST)
                         
         
                     if service_type == 'ELECTRICITY':
